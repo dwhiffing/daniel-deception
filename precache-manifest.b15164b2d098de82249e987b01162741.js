5a33a913d5c9ba99dd4a47ba244fa73bb0e43a82
Daniel Whiffing <danielwhiffing@gmail.com>
2020-10-31 20:21:47 -0400

    Updates

---
+++ precache-manifest.b15164b2d098de82249e987b01162741.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d446601587f56afb56b6d25db3c84ded",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74be297c6fdb971d9d01",
+    "url": "/static/js/2.4ce9afc6.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.4ce9afc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b5708ab0c16e204208",
+    "url": "/static/js/main.13e2f07f.chunk.js"
+  },
+  {
+    "revision": "be3988bba9c4d0b3e0a1",
+    "url": "/static/js/runtime-main.491959d8.js"
+  }
+]);
