f74d745f3907fe085c653169675f4f7b3988b4c3
Daniel Whiffing <danielwhiffing@gmail.com>
2020-11-01 16:58:35 -0500

    Refactor room part 1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { Room } from './screens/Room'
+import { Lobby } from './screens/Lobby'
 import { Client } from 'colyseus.js'
-
-// TODO: show guess made on each player
-// TODO: show if guess had at least one component correct
-// TODO: Ensure players don't leave if their phone sleeps
-// TODO: Allow configuration of reconnect time, phaseTimer, number of cards dealt
-// TODO: Send evidence marking over server with player color?
-// TODO: Ensure all secrets are removed?
 
 window.colyseus = new Client(
   process.env.NODE_ENV === 'production'
     ? 'wss://daniel-deception.herokuapp.com'
     : 'ws://localhost:3553',
 )
+
+function App() {
+  const [room, setRoom] = useState()
+  return room ? (
+    <Room room={room} setRoom={setRoom} />
+  ) : (
+    <Lobby setRoom={setRoom} />
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
